a563efe85c505c2fabb06a5ca86d0f031f0411ba
Facco99 <facchin810@gmail.com>
2020-06-16 10:57:28 +0200

    create all customer module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'customers', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, { path: 'customers', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) }, { path: 'customers', loadChildren: () => import('./features/todos/todos.module').then(m => m.TodosModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
