d374c17e27771865a9296063401735ff16d521cc
Facco99 <facchin810@gmail.com>
2020-06-24 12:53:49 +0200

    definizione primo selettore

--- src/app/redux/index.ts
+++ src/app/redux/index.ts
@@ -1,1 +1,23 @@
+import { TodoState } from './todos/todos.reducers';
+import { createSelector } from '@ngrx/store';
 
+export interface AppState {
+    todoState: TodoState;
+}
+
+export const selectTodosState = (state: AppState) => state.todoState;
+
+export const selectTodos = createSelector(
+    selectTodosState,
+    (state: TodoState) => state.todos
+);
+
+export const getTodoById = createSelector(
+    selectTodosState,
+    (state: TodoState, props: { id: number }) => state.todos.find(item => item.id === props.id)
+);
+
+export const getFirstTodo = createSelector(
+    selectTodosState,
+    (state: TodoState) => state.todos.length > 0 ? state.todos[0] : null
+);
--- src/app/redux/todos/todos.actions.ts
+++ src/app/redux/todos/todos.actions.ts
@@ -1,7 +1,7 @@
 import { createAction, props } from '@ngrx/store';
-import { Todo } from '../core/model/todo';
+import { Todo } from '../../core/model/todo';
 
 export const initTodos = createAction('[Todos] Init', props<{todos: Todo[]}>());
-export const insertTodo = createAction('[Todos] Insert', props<{todo: Todo[]}>());
+export const insertTodo = createAction('[Todos] Insert', props<{todo: Todo}>());
 export const removeTodo = createAction('[Todos] Remove', props<{id: number}>());
-export const editTodo = createAction('[Todos] Edit', props<{todo: Todo[]}>());
+export const editTodo = createAction('[Todos] Edit', props<{todo: Todo}>());
