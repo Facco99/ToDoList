e4f4c353fd57cb59863ae87275164f4e5f5d67c1
Facco99 <facchin810@gmail.com>
2020-06-28 10:26:07 +0200

    utilizzo delle subscription

--- src/app/features/todos/components/todo-edit/todo-edit.component.ts
+++ src/app/features/todos/components/todo-edit/todo-edit.component.ts
@@ -1,31 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Todo } from 'src/app/core/model/todo';
 import { ActivatedRoute } from '@angular/router';
 import { TodoFacadeService } from '../../services/todo-facade.service';
 import { Store, select } from '@ngrx/store';
 import { getTodoById } from 'src/app/redux';
+import { filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo-edit',
   templateUrl: './todo-edit.component.html',
   styleUrls: ['./todo-edit.component.scss']
 })
-export class TodoEditComponent implements OnInit {
+export class TodoEditComponent implements OnInit, OnDestroy {
 
+  private subscription: Subscription = new Subscription();
   todo:Todo;
 
   constructor(private todosFacadeService: TodoFacadeService, private route: ActivatedRoute, private store:Store) {
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      if (params != null && params['id'] != null) {
-        this.store.pipe(select(getTodoById, { id: Number(params['id']) })).subscribe(todo => {
-          this.todo = todo;
-        });
-      }
-    });
+    this.subscription.add(this.route.params.pipe(
+      filter(params => params != null && params['id'] != null),
+      switchMap(params => this.store.pipe(select(getTodoById, { id: Number(params['id']) }))),
+    ).subscribe(todo => {
+      this.todo = todo;
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   editForm(todo: Todo) {
