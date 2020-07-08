1f4c16d36999b2238d171f63439159ff9d3f410b
Facco99 <facchin810@gmail.com>
2020-07-08 09:52:29 +0200

    login con json server

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpCommunicationsService } from '../http-communications/http-communications.service';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { Todo } from '../model/todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private httpCommunications: HttpCommunicationsService) { }
+
+  doLogin(username:string): Observable<User[]>{
+    return this.httpCommunications.retrieveGetCall<User[]>("users", {username});
+  }
+}
--- src/app/features/login/services/login.service.ts
+++ src/app/features/login/services/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpCommunicationsService } from 'src/app/core/http-communications/http-communications.service';
 import { User } from 'src/app/core/model/user';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,38 +11,19 @@ export class LoginService {
 
   usersList: User[];
 
-  constructor(private router:Router, private myHttpService: HttpCommunicationsService) { 
-    this.getUsers();
+  constructor(private router:Router, private myHttpService: HttpCommunicationsService,private authService : AuthService) { 
   }
 
-  accesso(form): boolean{
-    let controllo=false;
-    this.usersList.forEach(element => {
-      if(element.username==form.username && element.password==form.password){
-        
-          sessionStorage.setItem('user', JSON.stringify(element.id));
-          
-        controllo=true;
+  executeLogin(username:string){
+    this.authService.doLogin(username).subscribe((users: User[]) => {
+      if (users && users.length > 0) {
+        sessionStorage.setItem("user", username);
+        this.router.navigateByUrl("/home");
+      }else{
+        alert("Login errata");
       }
-    });
-    return controllo;
-  }
-
-  executeLogin(form){
-    if (this.accesso(form)) {
-      this.router.navigateByUrl('/home');
-    }
-    else{
-      window.alert("utente non trovato")
-      this.router.navigateByUrl('/login');
-    }
-  }
-
-  getUsers(){
-    this.myHttpService.getUsers().subscribe(reponse => {
-      this.usersList = reponse;
-    }, err => {
-      console.log('error');
+    }, ()=>{
+      alert("Login in errore");
     });
   }
 }
